631bf690a573cd80c590e0e4e9bcb3f5201ba8af
Jimol1711 <juan.molina.l@ug.uchile.cl>
2025-04-27 12:04:38 -0400

    chore: progress on Lab6

--- Lab6/mdp_lab06/mdp-kafka/src/org/mdp/kafka/cli/BurstDetector.java
+++ Lab6/mdp_lab06/mdp-kafka/src/org/mdp/kafka/cli/BurstDetector.java
@@ -4,6 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
+
+import java.util.LinkedList;
+
 import java.util.Properties;
 import java.util.UUID;
 
@@ -13,8 +16,12 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.mdp.kafka.def.KafkaConstants;
 
-public class PrintEarthquakeTweets {
+public class BurstDetector {
     public static final String[] EARTHQUAKE_SUBSTRINGS = new String[] { "terremoto", "temblor", "sismo", "quake" };
+
+    public static final int FIFO_SIZE = 50;
+    public static final int EVENT_START_TIME_INTERVAL = 50 * 1000;
+    public static final int EVENT_END_TIME_INTERVAL = 2 * EVENT_START_TIME_INTERVAL;
 
     public static void main(String[] args) throws FileNotFoundException, IOException{
         if(args.length!=1){
@@ -32,6 +39,10 @@ public class PrintEarthquakeTweets {
 
         consumer.subscribe(Arrays.asList(args[0]));
 
+        LinkedList<ConsumerRecord<String, String>> fifo = new LinkedList<ConsumerRecord<String, String>>();
+        boolean inEvent = false;
+        int events = 0;
+
         try{
             while (true) {
                 // every ten milliseconds get all records in a batch
@@ -39,14 +50,29 @@ public class PrintEarthquakeTweets {
 
                 // for all records in the batch
                 for (ConsumerRecord<String, String> record : records) {
+                    fifo.add(record);
+
                     String lowercase = record.value().toLowerCase();
+
+                    if (fifo.size() >= FIFO_SIZE) {
+                        ConsumerRecord<String, String> oldest = fifo.removeFirst();
+                        long gap = record.timestamp() - oldest.timestamp();
+
+                        if (gap <= EVENT_START_TIME_INTERVAL && !inEvent) {
+                            inEvent = true;
+                            events++;
+                            System.out.println("START event-id:" + events + ": start:" + oldest.timestamp() + " value:" + oldest.value() + " rate:" + FIFO_SIZE + " records in " + gap + " ms");
+                        } else if (gap >= EVENT_TIME_INTERVAL && inEvent) {
+                            inEvent = false;
+                            System.out.println("END event:" + events + " rate:" + FIFO_SIZE + "records in " + gap + "ms");
+                        }
+                    }
 
                     // check if record value contains keyword
                     // (could be optimised a lot)
                     for(String ek: EARTHQUAKE_SUBSTRINGS){
                         // if so print it out to the console
                         if(lowercase.contains(ek)){
-                            // System.out.println(record.value());
                             // prevents multiple prints of the same tweet with multiple keywords
                             break;
                         }
